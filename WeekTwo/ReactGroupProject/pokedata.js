ede950a723f928dd30972ec90ded384841361158
Nicklas W. <Nicklas2002@hotmail.com>
2024-05-05 16:02:43 +0200

    nye pokemon

--- WeekTwo/ReactGroupProject/pokedata.js
+++ WeekTwo/ReactGroupProject/pokedata.js
@@ -7,7 +7,7 @@ async function fetchdata(id){
     await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((response => response.json())).then((data) => pokedata.pokemon.push(data)); 
 }
 
-for (let i = 1; i <= 151; i++){
+for (let i = 1; i <= 251; i++){
     await fetchdata(i);
     //await sleep(1000);
 }
